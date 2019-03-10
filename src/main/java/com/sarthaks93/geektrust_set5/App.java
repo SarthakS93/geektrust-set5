b474ef8465325df46269218393bc88891d64ec7c
sarthak <sarthaks93@gmail.com>
2019-03-10 21:02:11 +0530

    project init

---
+++ src/main/java/com/sarthaks93/geektrust_set5/App.java
@@ -0,0 +1,13 @@
+package com.sarthaks93.geektrust_set5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
